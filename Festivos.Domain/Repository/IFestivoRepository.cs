d580432c95983039330e667bfc8230daf4b43f78
MagicExist <johhanparra1@gmail.com>
2024-09-04 07:27:20 -0500

    Basic Stuff between repository layer added

---
+++ Festivos.Domain/Repository/IFestivoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festivos.Domain.Repository
+{
+    public interface IFestivoRepository
+    {
+        public Task<string> IsHoliday(int year, int month, int day);
+    }
+}
---
+++ Festivos.Persistence/Repositories/FestivoRepository.cs
@@ -0,0 +1,23 @@
+﻿using Festivos.API.Models;
+using Festivos.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festivos.Persistence.Repositories
+{
+    internal class FestivoRepository : IFestivoRepository
+    {
+        private readonly FestivosContext _ctx;
+        public FestivoRepository(FestivosContext ctx) 
+        {
+            _ctx = ctx;
+        }
+        public Task<string> IsHoliday(int year, int month, int day)
+        {
+            return null;
+        }
+    }
+}
